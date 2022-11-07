937e137b02541048aabc32e0ccabaf305572ad83
kunjan-tanna <kvtanna.om@gmail.com>
2022-11-07 10:35:26 +0530

    Changes

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,7 +2,7 @@ import axios from "./config";
 import { displayLog } from "./functions";
 
 export const apiCall = async (method, url, reqData, params) => {
-   let token = "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";
+   let token = "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";
    return axios({
       method: method,
       url: url,
